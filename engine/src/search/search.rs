c55985a92e755d4c281c4c981c12ba8bae6668f7
Ludovic <ludovicdebever0@gmail.com>
2025-04-14 21:11:46 +0200

    fix(lmr): attempt at fixing lmr
    Bench: 24346

--- engine/src/search/search.rs
+++ engine/src/search/search.rs
@@ -271,30 +271,24 @@ impl Search {
             self.make_move_nnue(&mut pos, m);
 
             let mut score: i32;
+            let mut r = 1;
 
+            if depth >= 2 && i >= 1 && !pos.is_check() {
+                r = match m {
+                    m if m.is_capture() || m.is_promotion() => {
+                        max(1, (0.7 + (depth as f64).ln() * (i as f64).ln() / 3.0) as u8)
+                    }
+                    _ => max(1, (0.7 + (depth as f64).ln() * (i as f64).ln() / 2.4) as u8),
+                };
+            }
             /* Principal Variation Search */
             match i {
                 0 => score = -self.negamax(&pos, depth - 1, -beta, -alpha, ply + 1),
                 _ => {
-                    if depth >= 2 && i >= 1 && !pos.is_check() {
-                        let r = match m {
-                            m if m.is_capture() || m.is_promotion() => {
-                                max(1, (0.7 + (depth as f64).ln() * (i as f64).ln() / 3.0) as u8)
-                            }
-                            _ => max(1, (0.7 + (depth as f64).ln() * (i as f64).ln() / 2.4) as u8),
-                        };
+                    score = -self.negamax(&pos, depth - r, -(alpha + 1), -alpha, ply + 1);
 
-                        score = -self.negamax(&pos, depth - r, -(alpha + 1), -alpha, ply + 1);
-                    } else {
-                        score = alpha + 1;
-                    }
-
-                    if score > alpha {
-                        score = -self.negamax(&pos, depth - 1, -(alpha + 1), -alpha, ply + 1);
-
-                        if score > alpha && score < beta {
-                            score = -self.negamax(&pos, depth - 1, -beta, -alpha, ply + 1);
-                        }
+                    if score > alpha && beta - alpha > 1 {
+                        score = -self.negamax(&pos, depth - 1, -beta, -alpha, ply + 1);
                     }
                 }
             }
